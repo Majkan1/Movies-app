35134b2d625cd38311086f3bc1caf4cae39e8615
Mikołaj Michalak <majkanart117@gmail.com>
2025-12-23 18:01:54 +0100

    Add new lines

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="app">
       <h1>Movies App</h1>
-      <p>Start here: edit src/App.jsx</p>
+    <p>This is my application about movies</p>
     </div>
   )
 }
