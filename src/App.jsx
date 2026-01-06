ba83f9e4d5252f1fa2c61f9343a1a5c73af05554
Mikołaj Michalak <majkanart117@gmail.com>
2026-01-06 16:09:30 +0100

    Add few changes.

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,17 @@ export default function App() {
 function Photo(){
   return (
     <div className = "Both">
-      <div className = "Black"></div>
-      <div>
-        <img className = "Photo" src={`${import.meta.env.BASE_URL}Photo.jpg`} alt="Image of Pizza"/>
+      <div className = "Black">
+        <p className = "Logo">My logo</p>
+        <h2 className = "Title">Restaurant of your dreams<br/>Enjoy the best dishes in the world</h2>
+        <div className = "underline"></div>
+        <p className = "under-title">Here you can try the best dishes in the world</p>
+        <button className = "Go-to-menu">Go to the menu →</button>
+      </div>
+      <div className = "Photo">
+        <img src={`${import.meta.env.BASE_URL}Photo.jpg`} alt="Image of Pizza"/>
+        <div className = "underline1"></div>
+        <div className = "underline2"></div>
       </div>
     </div>
   )
